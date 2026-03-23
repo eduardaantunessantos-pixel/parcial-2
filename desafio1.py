f1c8709db6ada8293b794ad0534c32cb02630860
Eduarda Antunes <eduarda.antunes.santos@escola.pr.gov.br>
2026-03-23 12:10:38 -0300

    Add files via upload

---
+++ desafio1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Programa que soma dois números
+
+# Solicita ao usuário o primeiro número
+numero1 = float(input("Digite o primeiro número: "))
+
+# Solicita ao usuário o segundo número
+numero2 = float(input("Digite o segundo número: "))
+
+# Calcula a soma
+soma = numero1 + numero2
+
+# Exibe o resultado
+print("A soma dos dois números é:", soma)

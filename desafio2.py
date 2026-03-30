1038633ae69eba2765090432fc5075ae6c929ff4
Eduarda Antunes <eduarda.antunes.santos@escola.pr.gov.br>
2026-03-30 07:52:08 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,7 +1,9 @@
 # Solicita que o usuário digite um número e o converte para inteiro
 numero = int(input("Digite um número: "))
+
 # Verifica se o número é divisível por 2 (ou seja, se é par)
 if numero % 2 == 0:
+    
 # Se o número for ímpar (resto da divisão por 2 não é 0), imprime "Ímpar    print("Par")
 else:
     print("Ímpar")

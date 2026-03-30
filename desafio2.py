b4669643f215b171d9afa02a535ef685dafedf6a
Eduarda Antunes <eduarda.antunes.santos@escola.pr.gov.br>
2026-03-30 07:51:31 -0300

    Update desafio2.py

--- desafio2.py
+++ desafio2.py
@@ -1,8 +1,7 @@
-# Programa simples para checar se um número é par
-
+# Solicita que o usuário digite um número e o converte para inteiro
 numero = int(input("Digite um número: "))
-
+# Verifica se o número é divisível por 2 (ou seja, se é par)
 if numero % 2 == 0:
-    print("Par")
+# Se o número for ímpar (resto da divisão por 2 não é 0), imprime "Ímpar    print("Par")
 else:
     print("Ímpar")
